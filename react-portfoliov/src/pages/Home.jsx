a8b6c26cd6dd1252947b41308164d621fd5bf15b
Snalepa <snalepa1998@gmail.com>
2024-05-06 18:23:28 -0400

    added headshot

--- react-portfoliov/src/pages/Home.jsx
+++ react-portfoliov/src/pages/Home.jsx
@@ -1,26 +1,15 @@
+import headshot from '../assets/headshot.jpeg'
+
 export default function Home() {
     return (
       <div>
-        <nav>
-        <h1>Home Page</h1>
-        <ul>
-            <li>About Me</li>
-            <li>Portfolio</li>
-            <li>Resume</li>
-            <li>Contact</li>
-        </ul>
-        </nav>
+        <h1>About Me</h1>
+        <img src={headshot} alt="headshot" />
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-          ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-          non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-          ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-          rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-          tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-          porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-          vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus.
+         Hello and welcome to my webpage! My name is Sarah and I am an aspiring software developer. I
+        graduated with a Bachelor's and Master's in International Relations and have previously worked in
+        the not for profit space. I am currently taking the Northwestern Full-Stack Bootcamp and look
+        forward to a long and prosperous career in tech!
         </p>
       </div>
     );
