fb97a6c8be350c3fbaaafd486262d7b1accf5922
Snalepa <snalepa1998@gmail.com>
2024-05-07 13:16:43 -0400

    footer

--- react-portfoliov/src/App.jsx
+++ react-portfoliov/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
   
-      {/* <Home></Home>  */}
+      {/* <Home></Home>*/}
       {/* <Resume></Resume> */}
       {/* <Portfolio></Portfolio> */}
       {/* <Header></Header> */}
--- react-portfoliov/src/pages/Footer.jsx
+++ react-portfoliov/src/pages/Footer.jsx
@@ -1,10 +1,14 @@
 import '../App.css'
-import github from '../assets/github.jpg'
+import github from '../assets/GH.jpeg'
+import stack from '../assets/stack.png'
+import LinkedIn from '../assets/LI.png'
 
 export default function Footer(){
     return (
         <footer>
             <img src={github} alt='github logo'></img>
+            <img src={stack} alt='stack overflow icon'></img>
+            <img src={LinkedIn} alt='LinkedIn logo'></img>
         </footer>
     );
 }
