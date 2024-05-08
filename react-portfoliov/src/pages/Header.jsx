6e054f1a5d2cde81e773c983cbeabcb8a4be5047
Snalepa <snalepa1998@gmail.com>
2024-05-08 11:08:47 -0400

    Header

--- react-portfoliov/src/pages/Header.jsx
+++ react-portfoliov/src/pages/Header.jsx
@@ -2,14 +2,18 @@ import '../App.css'
 
 export default function Header(){
     return(
+        <div>
         <header class='flex'>
          <h1>Sarah Nalepa</h1>
+         <div>
          <ul class='flex' id='header'>
             <li>About Me</li>
             <li>Portfolio</li>
             <li>Contact</li>
             <li>Resume</li>
          </ul>
+         </div>
         </header>
+        </div>
     );
 }
