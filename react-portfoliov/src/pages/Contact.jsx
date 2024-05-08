e98cfbaacd455ed401d5c0569909a5e229ab8b40
Snalepa <snalepa1998@gmail.com>
2024-05-08 19:32:08 -0400

    Adding basic layout for the contact page

---
+++ react-portfoliov/src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+export default function Contact(){
+    return(
+        <div>
+            <h1>Contact Form</h1>
+
+            <div>
+            
+            </div>
+        </div>
+    );
+}
